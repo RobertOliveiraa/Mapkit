2bbe2b6e8d65a033ef8426bbeaa7d444800de322
Edlberto Ramos <edilberto.ramos@icloud.com>
2015-05-13 01:03:09 -0400

    Coloquei o pino na localização do usuário.

--- MapKit/MapKit/MapKitViewController.m
+++ MapKit/MapKit/MapKitViewController.m
@@ -28,22 +28,6 @@
     self.mapa.showsUserLocation = YES;
     [self.locationManager requestWhenInUseAuthorization];
     
-//    //Por pino no mapa
-//    //for (int i = 0; i < results.count; i++) {
-//    //adiciona um pin em uma certa latitude e longitude.
-//    CLLocationCoordinate2D annotationCoord;
-//
-//    //CLLocationDegrees lat = [[results[i] valueForKey:@"latitude"] doubleValue];
-//    //CLLocationDegrees longi = [[results[i] valueForKey:@"longitude"] doubleValue];
-//    annotationCoord.latitude = 50.44444;
-//    annotationCoord.longitude = -3.44545;
-//    MKPointAnnotation *annotationPoint = [[MKPointAnnotation alloc] init];
-//    annotationPoint.coordinate = annotationCoord;
-//    //annotationPoint.title = self.title;
-//    //annotationPoint.subtitle = self.subtitle;
-//
-//    [self.mapa addAnnotation:annotationPoint];
-//        //    }
     
     //[self.locationManager requestAlwaysAuthorization];
     UILongPressGestureRecognizer *lpgr = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
@@ -59,6 +43,24 @@
     
     NSLog(@"\n\ncurrent: %f", userLocation.location.coordinate.latitude);
     NSLog(@"\n\ncurrent: %f", userLocation.location.coordinate.longitude);
+    //    //Por pino no mapa
+    //    //for (int i = 0; i < results.count; i++) {
+    //    //adiciona um pin em uma certa latitude e longitude.
+        CLLocationCoordinate2D annotationCoord;
+    //
+    //    //CLLocationDegrees lat = [[results[i] valueForKey:@"latitude"] doubleValue];
+    //    //CLLocationDegrees longi = [[results[i] valueForKey:@"longitude"] doubleValue];
+        annotationCoord.latitude =  userLocation.location.coordinate.latitude;
+        annotationCoord.longitude =  userLocation.location.coordinate.longitude;
+        MKPointAnnotation *annotationPoint = [[MKPointAnnotation alloc] init];
+        annotationPoint.coordinate = annotationCoord;
+    //    //annotationPoint.title = self.title;
+    //    //annotationPoint.subtitle = self.subtitle;
+    //
+        [self.mapa addAnnotation:annotationPoint];
+    //        //    }
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
